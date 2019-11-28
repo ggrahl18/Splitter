de3bdee5e34df03e545052869a82cb6ef43a0460
ggrahl18 <ggrahl18@gmail.com>
2019-11-28 13:25:47 -0600

    alice decleration fixed

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -2,30 +2,30 @@ pragma solidity ^0.5.12;
 
 contract Ownable {
 
-    address private _alice;
+    address payable public alice;
 
     event LogNewOwner(address indexed sender, address indexed originalAlice, address indexed newAlice);
 
     constructor() public {
-        _alice = msg.sender;
+        alice = msg.sender;
     }
 
     modifier onlyAlice() {
         require(
-            msg.sender == _alice,
+            msg.sender == alice,
             "Only owner can call this function."
         );
         _;
     }
 
-    function changeAlice(address _newAlice) public onlyAlice returns(bool success) {
+    function changeAlice(address payable _newAlice) public onlyAlice returns(bool success) {
 		require(_newAlice != address(0), "The new Alice cannot be the same as the original Alice.");
-        emit LogNewOwner(msg.sender, _alice, _newAlice);
-		_alice = _newAlice;
+        emit LogNewOwner(msg.sender, alice, _newAlice);
+		alice = _newAlice;
         return true;
 	}
 
     function getAlice() public view returns (address) {
-        return _alice;
+        return alice;
     }
 }
