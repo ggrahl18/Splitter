0df43c7dd9c2252bdf8a494631f6d814dac3933f
ggrahl18 <ggrahl18@gmail.com>
2020-02-12 09:39:19 -0600

    Updated truffle-config for simplicity

--- truffle-config.js
+++ truffle-config.js
@@ -1,27 +1,15 @@
+const path = require("path");
 
-// Uncommenting the defaults below
-// provides for an easier quick-start with Ganache.
-// You can also follow this format for other networks;
-// see <http://truffleframework.com/docs/advanced/configuration>
-// for more details on how to specify configuration options!
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    development: { // This one is optional and reduces the scope for failing fast
-      host: "localhost",
-      port: 7545,
-      network_id: "*", // Match any network id
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // match any network
       gas: 5000000
     },
-    net42: {
-      host: "localhost",
-      port: 8545,
-      network_id: 42,
-      gas: 5000000
-    }
-  },
-  compilers: {
-    solc: {
-      version: "^0.5.12"
-    }
   }
 };
